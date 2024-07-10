c0caa67406a5a1e59bc07b357fc5b776356e949c
Fizzchimp <callumaskew@gmail.com>
2024-07-10 23:44:17 +0100

    organised

--- main.py
+++ main.py
@@ -1,50 +1,93 @@
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
 from numpy import pi, sin, cos
+import pygame as pg
+from display import Display
 
-dt = 0.001
+class World():
+    def __init__(self):
+        pg.init()
+        self.display = Display()
 
-# velocity = int(input("Enter start velocity\n: "))
-# angle = int(input("\n\nEnter angle\n: ")) / 180 * pi
-# height = int(input("\n\nEnter starting height\n: "))
-# gravity = int(input("\n\nEnter gravity\n: "))
+    def basicProj(self):
 
-velocity = 10
-angle = 45 / 180 * pi
-height = 10
-gravity = 10
+        # velocity = int(input("Enter start velocity\n: "))
+        # angle = int(input("\n\nEnter angle\n: ")) / 180 * pi
+        # height = int(input("\n\nEnter starting height\n: "))
+        # gravity = int(input("\n\nEnter gravity\n: "))
 
-x = 0
-y = height
+        velocity = 10
+        angle = 45 / 180 * pi
+        height = 10
+        gravity = 10
 
-xVel = velocity * cos(angle)
-yVel = velocity * sin(angle)
+        x = 0
+        y = height
 
-xAcc = 0
-yAcc = -gravity
+        xVel = velocity * cos(angle)
+        yVel = velocity * sin(angle)
 
-yPoints = []
-xPoints = []
+        xAcc = 0
+        yAcc = -gravity
 
-xDif = 0.0001
-i = 0
-while y >= 0:
-    x = i * xDif
-    t = x / xVel
+        self.points = []
+
+        xDif = 0.01
+        i = 0
+        while y >= 0:
+            x = i * xDif
+            t = x / xVel
+            
+            y = yVel * t + 0.5 * yAcc * t * t + height
+
+            self.points.append((x, y))
+
+            i += 1
     
-    y = yVel * t + 0.5 * yAcc * t * t + height
 
-    xPoints.append(x)
-    yPoints.append(y)
-
-    i += 1
-    
-
-# Finding the apogee
-maxTime = -yVel / yAcc
-apogee = (xVel * maxTime + 0.5 * xAcc * maxTime ** 2, yVel * maxTime + 0.5 * yAcc * maxTime ** 2 + height)
+        # Finding the apogee
+        maxTime = -yVel / yAcc
+        self.apogee = (xVel * maxTime + 0.5 * xAcc * maxTime ** 2, yVel * maxTime + 0.5 * yAcc * maxTime ** 2 + height)
 
 
-plt.plot(xPoints, yPoints)
-plt.plot(apogee[0], apogee[1], "ro")
-plt.show()
+    def doEvents(self):
+        for event in pg.event.get():
+                if event.type == pg.QUIT: self.running = False
+                 
+                # Scroll Wheel Input
+                if event.type == pg.MOUSEWHEEL:
+                    if event.y == 1:
+                        self.display.graphZoom /= 1.5
+                    if event.y == -1:
+                        self.display.graphZoom *= 1.5
+
+                # Mouse Button Inputs
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    self.mouseTracking = True
+                    pg.mouse.get_rel()
+                if event.type == pg.MOUSEBUTTONUP: self.mouseTracking = False
+
+                # Keyboard Inputs
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_ESCAPE: self.running = False
+
+
+        if self.mouseTracking:
+            displacement = pg.mouse.get_rel()
+            self.display.graphCentre[0] += displacement[0]
+            self.display.graphCentre[1] += displacement[1]
+
+    def run(self):
+        self.running = True
+        self.mouseTracking = False
+        while self.running:
+            
+            self.doEvents()
+            self.display.drawScreen(self.points)
+
+world = World()
+# world.basicProj()
+
+world.points = []
+for i in range(720):
+    world.points.append((i / 180 * pi, sin(i / 180 * pi)))
+
+world.run()
